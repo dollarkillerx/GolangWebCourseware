d9eee4bbda856b070f22d8994dd1683b2cafd12e
Dollarkillerx <dollarkiller@vip.qq.com>
2019-05-26 22:16:12 +0800

    init

--- defs/defs.go
+++
@@ -1,6 +0,0 @@
-package defs
-
-const (
-	FILE_DIR = "./file/"
-	MAX_UPLOAD_SIZE = 1024 * 1024 * 50
-)
--- defs/request.go
+++
@@ -1,6 +0,0 @@
-package defs
-
-type User struct {
-	Name string `json:"name"`
-	Password string `json:"password"`
-}
